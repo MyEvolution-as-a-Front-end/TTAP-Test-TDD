c35495969825b082235ae72518b2dd39f3a5ee3d
JohnAnon9771 <njoao97710@gmail.com>
2021-07-08 15:18:49 -0300

    :tada: initial commit

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
+  moduleNameMapper: {
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+  },
+  moduleDirectories: ["node_modules", "src"],
+};
